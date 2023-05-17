f787b742b978e5fb2535cac67e9fbd8e60eddedf
+ <+>
2023-05-17 10:26:43 +0300

    commit

--- landingpage/src/components/footer.jsx
+++ landingpage/src/components/footer.jsx
@@ -44,6 +44,7 @@ export default function Footer() {
           <p>Support Us</p>
           <p></p>
         </div>
+      
       </div>
       <div className="mx-[10%] pb-5 flex text-[.5rem] md:text-[.72rem] mt-10">
         <p className="basis-2/6 md:basis-6/12">@2021 Homyz. <span className="block md:inline">All rights reserved</span></p>
