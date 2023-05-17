c3bf3f6e839cfcc14ddec8dc898f333c1687fe37
+ <+>
2023-05-17 10:41:59 +0300

    commit

--- landingpage/src/components/getstarted.jsx
+++ landingpage/src/components/getstarted.jsx
@@ -1,7 +1,8 @@
 
 export default function Getstarted() {
   return (
-    <div className="mx-[10%] text-white rounded-xl my-16 p-7 text-center bg-gradient-to-r relative to-[#2746c4] from-[#3b5cda] ">
+   
+   <div className="mx-[10%] text-white rounded-xl my-16 p-7 text-center bg-gradient-to-r relative to-[#2746c4] from-[#3b5cda] ">
       <div className="absolute -inset-4 rounded-3xl  bg-gradient-to-r -z-10 to-[#2346d3] from-[#5974e0]"></div>
       <h1 className="mt-12 mb-8 text-5xl font-bold">Get Started with Homyz</h1>
       <p>Subscribe and find super attractive prices qoptws</p>
